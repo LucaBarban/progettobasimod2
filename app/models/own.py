8343c9fe03fbd9556d27f40ab00e6eaba2c7d8f9
Friedchicken-42 <paololego20+github@gmail.com>
2024-05-10 18:05:06 +0200

    Updated `Book` fields name

--- app/models/own.py
+++ app/models/own.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, UniqueConstraint
+from sqlalchemy import ForeignKey, Integer, String, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database import Base
