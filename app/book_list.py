a2be1108ea9a772d70bbf1ffc2834ed05e39b6a2
Luca Saccarola <github.e41mv@aleeas.com>
2024-05-17 15:16:27 +0200

    Add first implementation of homepage

---
+++ app/book_list.py
@@ -0,0 +1,27 @@
+from random import shuffle
+from typing import Callable, List, Self
+
+from app.database import db
+from app.models.book import Book
+from app.models.genre import Genre
+
+
+class BookListByGenre:
+    genres: List[Genre] = []
+
+    def __init__(self) -> None:
+        self.genres = db.session.query(Genre).all()
+        for x in self.genres:
+            shuffle(x.books)
+
+    def prune(self, predicate: Callable[[List[Book]], bool]) -> Self:
+        self.genres = [v for v in self.genres if not predicate(v.books)]
+        return self
+
+    def normalize(self, apply: Callable[[List[Book]], List[Book]]) -> Self:
+        for v in self.genres:
+            v.books = apply(v.books)
+        return self
+
+    def dump(self) -> List[Genre]:
+        return self.genres
--- app/routes/index.py
+++ app/routes/index.py
@@ -1,7 +1,15 @@
+from flask import current_app as app
+from flask import render_template
+
+from app.book_list import BookListByGenre
 from app.routes.auth import getLoggedInUser
-from flask import current_app as app, render_template
+
 
 @app.route("/")
 def index() -> str:
     user = getLoggedInUser()
-    return render_template("index.html", user=user)
+    books = BookListByGenre()
+    books.prune(lambda x: len(x) < 5).normalize(
+        lambda v: [x for i, x in enumerate(v) if i < 5]
+    )
+    return render_template("index.html", user=user, books=books.dump())
