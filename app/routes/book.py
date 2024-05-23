0b92bb272505eb0166f38ec9efab0fc08746af38
LucaBarban <45258881+LucaBarban@users.noreply.github.com>
2024-05-23 11:46:01 +0200

    Adding add*-pages

--- app/routes/book.py
+++ app/routes/book.py
@@ -13,12 +13,11 @@ import sqlalchemy as sq
 from sqlalchemy import exc
 
 @app.route("/book/")
-def products() -> str:
-    books = db.session.scalars(sq.select(Book))
+def products() -> Response:
 
-    return render_template("books.html", books=books)
+    return redirect("/library/")
 
-@app.route("/book/add", methods=['GET', 'POST'])
+@app.route("/book/add/", methods=['GET', 'POST'])
 def add() -> str|Response:
     usr: User|None = getLoggedInUser()
     if usr is None:
@@ -29,7 +28,7 @@ def add() -> str|Response:
     publishers = db.session.scalars(sq.select(Publisher)).fetchall()
 
     if request.method != 'POST':
-        return render_template("bookadd.html", user=usr, genres=genres, authors=authors, publishers=publishers)
+        return render_template("addbook.html", user=usr, genres=genres, authors=authors, publishers=publishers)
 
     title: str|None = request.form.get("title") or None
     published: date|None
@@ -59,14 +58,14 @@ def add() -> str|Response:
     if len(selectedGenres) == 0:
         flash("Select at least one genre")
     if title is None or published is None or pages <= 0 or isbn is None or authorid is None or publishername is None or len(selectedGenres) == 0:
-        return render_template("bookadd.html", user=usr, genres=genres, authors=authors, publishers=publishers)
+        return render_template("addbook.html", user=usr, genres=genres, authors=authors, publishers=publishers)
 
     book: Book
     author: Author|None = next((a for a in authors if a.id == authorid), None) # next iterates untill the iterator provided by the foor loop iterates afer the whole list
     publisher: Publisher|None = next((p for p in publishers if p.name == publishername), None)
     if author is None or publisher is None:
         flash("An unforeseen error occurred")
-        return render_template("bookadd.html", user=usr, genres=genres, authors=authors, publishers=publishers)
+        return render_template("addbook.html", user=usr, genres=genres, authors=authors, publishers=publishers)
 
     try:
         book = Book(title, published, pages, isbn, author, publisher)
@@ -76,7 +75,7 @@ def add() -> str|Response:
         db.session.rollback()
         flash("An error occured while adding the new book")
 
-    return render_template("bookadd.html", user=usr, genres=genres, authors=authors, publishers=publishers)
+    return render_template("addbook.html", user=usr, genres=genres, authors=authors, publishers=publishers)
 
 @app.route("/book/<int:id>")
 def get(id: int) -> str:
@@ -85,13 +84,81 @@ def get(id: int) -> str:
 
     return render_template("book.html", book=book)
 
+@app.route("/book/add/genre")
+def addgenre() -> str|Response:
+    usr: User|None = getLoggedInUser()
+    if usr is None:
+        return redirect("/login/?link=/book/add/genre")
+
+    if request.method == 'POST':
+        genrename: str|None = request.form.get("name") or None
+        if genrename is None:
+            flash("You must compile the genre field")
+        else:
+            try:
+                genre = Genre(genrename)
+                db.session.add(genre)
+                db.session.commit()
+            except exc.SQLAlchemyError as e:
+                db.session.rollback()
+                flash("An error occured while adding the new genre")
+
+    return render_template("addgenre.html", user=usr)
+
+@app.route("/book/add/publisher")
+def addpublisher() -> str|Response:
+    usr: User|None = getLoggedInUser()
+    if usr is None:
+        return redirect("/login/?link=/book/add/publisher")
+
+    if request.method == 'POST':
+        publishername: str|None = request.form.get("name") or None
+        if publishername is None:
+            flash("You must compile the publisher field")
+        else:
+            try:
+                publisher = Publisher(publishername)
+                db.session.add(publisher)
+                db.session.commit()
+            except exc.SQLAlchemyError as e:
+                db.session.rollback()
+                flash("An error occured while adding the new publisher")
+
+    return render_template("addpublisher.html", user=usr)
+
+@app.route("/book/add/author")
+def addauthor() -> str|Response:
+    usr: User|None = getLoggedInUser()
+    if usr is None:
+        return redirect("/login/?link=/book/add/author")
+
+    if request.method == 'POST':
+        first_name: str|None = request.form.get("first_name") or None
+        last_name: str|None = request.form.get("last_name") or None
+
+        if first_name is None or last_name is None:
+            flash("You must compile all the fields")
+        else:
+            try:
+                author = Author(first_name, last_name)
+                db.session.add(author)
+                db.session.commit()
+            except exc.SQLAlchemyError as e:
+                db.session.rollback()
+                flash("An error occured while adding the new author")
+
+    return render_template("addauthor.html", user=usr)
+
 def checkIsbn(isbn:str) -> bool:
     """
     Function used to check if a given ISBN code is correct. This
     has been borrowed from https://stackoverflow.com/a/4047709
     """
     isbn = isbn.replace("-", "").replace(" ", "").upper()
-    match = re.search(r'^(\d{9})(\d|X)$', isbn)
+    match = re.search(r'^(\d{9})(\d|X)$', isbn) # treating the string as raw (things like \ are treated as literal characters)
+                                                # from the start of the string (^) take 9 characters (\d{9}) and a tenth that
+                                                # can also be an X
+
     if not match:
         return False
 
