c1443ce252d90a010745445585bb6a01e46b23b6
LucaBarban <45258881+LucaBarban@users.noreply.github.com>
2024-05-23 10:36:57 +0200

    Possible fix for foreign keys

--- app/models/book.py
+++ app/models/book.py
@@ -1,7 +1,7 @@
 from datetime import date
 
 from sqlalchemy import ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database import Base
 from app.models.author import Author
@@ -19,13 +19,16 @@ class Book(Base):
     fk_author: Mapped[int] = mapped_column(ForeignKey(Author.id))
     fk_publisher: Mapped[str] = mapped_column(ForeignKey(Publisher.name))
 
-    def __init__(self, title:str, published:date, pages:int, isbn:str, fk_author:int, fk_publisher:int):
+    author: Mapped[Author] = relationship(Author)
+    publisher: Mapped[Publisher] = relationship(Publisher)
+
+    def __init__(self, title:str, published:date, pages:int, isbn:str, author:Author, publisher:Publisher):
         self.title = title
         self.published = published
         self.pages = pages
         self.isbn = isbn
-        self.fk_author = fk_author
-        self.fk_publisher = fk_publisher
+        self.author = author
+        self.publisher = publisher
 
     def __repr__(self) -> str:
         return f"Book {{{self.id},{self.title},{self.published},{self.pages},{self.isbn},{self.fk_author},{self.fk_publisher}}}"
