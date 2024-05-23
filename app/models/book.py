2e35197a205319b4a1d75b83ea24bc4d8b608de3
LucaBarban <45258881+LucaBarban@users.noreply.github.com>
2024-05-23 09:40:52 +0200

    Fixing isbn type in Book (unrelated fk fix required)

--- app/models/book.py
+++ app/models/book.py
@@ -15,9 +15,17 @@ class Book(Base):
     title: Mapped[str]
     published: Mapped[date]
     pages: Mapped[int]
-    isbn: Mapped[int]
+    isbn: Mapped[str]
     fk_author: Mapped[int] = mapped_column(ForeignKey(Author.id))
     fk_publisher: Mapped[str] = mapped_column(ForeignKey(Publisher.name))
+
+    def __init__(self, title:str, published:date, pages:int, isbn:str, fk_author:int, fk_publisher:int):
+        self.title = title
+        self.published = published
+        self.pages = pages
+        self.isbn = isbn
+        self.fk_author = fk_author
+        self.fk_publisher = fk_publisher
 
     def __repr__(self) -> str:
         return f"Book {{{self.id},{self.title},{self.published},{self.pages},{self.isbn},{self.fk_author},{self.fk_publisher}}}"
