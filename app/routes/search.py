ca0b2e7ac7402d6d153404f1191a23cd5b189b5b
Friedchicken-42 <paololego20+github@gmail.com>
2024-06-17 18:21:23 +0200

    Added Search filters, moved searchbar form to layout

--- app/routes/search.py
+++ app/routes/search.py
@@ -1,4 +1,4 @@
-from typing import Set
+from typing import List
 
 from flask import current_app as app
 from flask import redirect, render_template, request
@@ -8,29 +8,51 @@ from werkzeug.wrappers.response import Response
 from app.database import db
 from app.models.author import Author
 from app.models.book import Book
+from app.models.genre import Genre
 from app.models.publisher import Publisher
 
 
-def generate_book_list(query: str) -> Set[Book]:
-    return set(
-        db.session.query(Book)
-        .filter(
-            or_(
-                Book.title.icontains(query),
-                Book.author.has(Author.first_name.icontains(query)),
-                Book.author.has(Author.last_name.icontains(query)),
-                Book.publisher.has(Publisher.name.icontains(query)),
-            )
+def generate_book_list(
+    input: str, genres: List[str], publishers: List[str]
+) -> List[Book]:
+    query = db.session.query(Book).filter(
+        or_(
+            Book.title.icontains(input),
+            Book.author.has(Author.first_name.icontains(input)),
+            Book.author.has(Author.last_name.icontains(input)),
+            Book.publisher.has(Publisher.name.icontains(input)),
         )
-        .all()
     )
+
+    if len(genres):
+        query = query.filter(Book.genres.any(Genre.name.in_(genres)))
+    if len(publishers):
+        query = query.filter(Book.publisher.in_(publishers))
+
+    return query.all()
 
 
 @app.route("/search/", methods=["POST"])
 def search() -> str | Response:
     user_input = request.form.get("user_input")
+
     if request.method != "POST" or user_input is None:
         return redirect("#")
 
-    books = generate_book_list(user_input)
-    return render_template("search.html", user=None, books=books, query=user_input)
+    genres_input = request.form.getlist("genre")
+    publishers_input = request.form.getlist("publisher")
+
+    books = generate_book_list(user_input, genres_input, publishers_input)
+
+    genres = db.session.query(Genre).all()
+
+    publishers = db.session.query(Publisher).all()
+
+    return render_template(
+        "search.html",
+        user=None,
+        books=books,
+        query=user_input,
+        genres=genres,
+        publishers=publishers,
+    )
